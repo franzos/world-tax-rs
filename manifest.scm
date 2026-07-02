97237d62595ddbae56c2e4667fd76bdfc8b8b9ef
Franz Geffke <mail@gofranz.com>
2026-07-02 22:44:34 +0100

    chore: guix build manifest and direnv setup

--- manifest.scm
+++ manifest.scm
@@ -1,1 +1,35 @@
-(specifications->manifest '("rust" "rust-cargo" "gcc-toolchain"))
+(use-modules (guix packages)
+             (guix search-paths)
+             (gnu packages rust)
+             (gnu packages commencement)
+             (gnu packages tls)
+             (gnu packages nss))
+
+(define openssl-with-dir
+  (package
+    (inherit openssl)
+    (native-search-paths
+     (cons (search-path-specification
+            (variable "OPENSSL_DIR")
+            (files '("."))
+            (file-type 'directory)
+            (separator #f))
+           (package-native-search-paths openssl)))))
+
+(define gcc-toolchain-with-cc
+  (package
+    (inherit gcc-toolchain)
+    (native-search-paths
+     (cons (search-path-specification
+            (variable "CC")
+            (files '("bin/gcc"))
+            (file-type 'regular)
+            (separator #f))
+           (package-native-search-paths gcc-toolchain)))))
+
+(packages->manifest
+ (list rust
+       (list rust "cargo")
+       gcc-toolchain-with-cc
+       openssl-with-dir
+       nss-certs))
